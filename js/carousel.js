25ae02acc1b7d248d0f1f5673a9f029862decd80
Mary <mary.gansallo@gmail.com>
2017-11-03 07:51:46 -0400

    updated portfolio site

---
+++ js/carousel.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $(".hifi-images").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: true,
+    nextArrow: '<div class="sec1-right-icon-background"><span>&rsaquo;</span></div>',
+    prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
+    fade: true
+  });
+  $(".lofi").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: true,
+    nextArrow: '<div class="sec1-right-icon-background"><span>&rsaquo;</span></div>',
+    prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
+    fade: true
+  });
+  $(".turtles").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: true,
+    nextArrow: '<div class="sec1-right-icon-background"><span>&rsaquo;</span></div>',
+    prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
+    fade: true
+  });
+  $(".wireframes").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: true,
+    nextArrow: '<div class="sec1-right-icon-background"><span>&rsaquo;</span></div>',
+    prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
+    fade: true
+  });
+});
