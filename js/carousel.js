99ad0b84fcabcd69303974b1cf409f5be6e7f572
Mary <mary.gansallo@gmail.com>
2017-11-03 22:08:50 -0400

    added missing images in cultiv8 section; added a my skills section in the Design process

--- js/carousel.js
+++ js/carousel.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
     prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
     fade: true
   });
+  $(".hifi-images2").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: true,
+    nextArrow: '<div class="sec1-right-icon-background"><span>&rsaquo;</span></div>',
+    prevArrow: '<div class="sec1-left-icon-background"><span>&lsaquo;</span></div>',
+    fade: true
+  });
   $(".lofi").slick({
     dots: true,
     infinite: true,
